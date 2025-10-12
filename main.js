3e8b71c4ba9051e4770271080a704964b62ffa98
cive202 <civecodes@gmail.com>
2025-10-12 21:46:10 +0545

    Defining-road

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 // getContex("2d") returns a class which has all the 2d drawing tools
-const road = new Road (canvas.width/2,canvas.width)
+const road = new Road (canvas.width/2,canvas.width*0.9)
 const car = new Car(road.getLaneCenter(1),100,30,50);
 
 animate();  
--- road.js
+++ road.js
@@ -11,6 +11,14 @@ class Road {
         this.top = -infinity;
         this.bottom =  infinity;
 
+        const topLeft = {x:this.left,y:this.top}
+        const bottomLeft = {x:this.left,y:this.bottom}
+        const topRight = {x:this.right,y:this.top}
+        const bottomRight = {x:this.right,y:this.bottom}
+        this.borders = [
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ]
     }
 
     getLaneCenter(laneIndex){
@@ -23,21 +31,24 @@ class Road {
     
         for(let i=0;i<=this.laneCount;i++){
             //Linear interpolatoion
-            const x = lerp(
+            const x  = lerp(
                 this.left,
                 this.right,
                 i/this.laneCount
             );
-            if(i > 0 && i < this.laneCount){
-                ctx.setLineDash([20,20])
-            }
-            else{
-                ctx.setLineDash([])
-            }
+            
+            ctx.setLineDash([20,20])
             ctx.beginPath();
             ctx.moveTo(x, this.top)
             ctx.lineTo(x, this.bottom)
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border =>{
+                ctx.beginPath();
+                ctx.moveTo(border[0].x,border[0].y)
+                ctx.lineTo(border[1].x,border[1].y)
+                ctx.stroke();
+        })
     }
 }
