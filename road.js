7903d354e748278b78075797d101ba9c8c4dfc8e
cive202 <civecodes@gmail.com>
2025-10-12 17:33:01 +0545

    Creating-lanes

--- road.js
+++ road.js
@@ -6,7 +6,7 @@ class Road {
         this.left = x - width/2
         this.right = x + width/2
 
-        const infinity = 1000000000;
+        const infinity = 100000;
 
         this.top = -infinity;
         this.bottom =  infinity;
@@ -15,15 +15,24 @@ class Road {
     draw(ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
-
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top)
-        ctx.lineTo(this.left, this.bottom)
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top)
-        ctx.lineTo(this.right, this.bottom)
-        ctx.stroke()
+    
+        for(let i=0;i<=this.laneCount;i++){
+            //Linear interpolatoion
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            if(i > 0 && i < this.laneCount){
+                ctx.setLineDash([20,20])
+            }
+            else{
+                ctx.setLineDash([])
+            }
+            ctx.beginPath();
+            ctx.moveTo(x, this.top)
+            ctx.lineTo(x, this.bottom)
+            ctx.stroke();
+        }
     }
 }
