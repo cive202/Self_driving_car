09a18b79ebfbdb7a540090ce5fbeb2c633c57842
cive202 <civecodes@gmail.com>
2025-10-12 16:04:31 +0545

    make Road

--- main.js
+++ main.js
@@ -5,16 +5,15 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 // getContex("2d") returns a class which has all the 2d drawing tools
-console.log(ctx)
+const road = new Road (canvas.width/2,canvas.width)
 const car = new Car(100,100,30,50);
-
-car.draw(ctx);
 
 animate();  
 
 function animate(){
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
