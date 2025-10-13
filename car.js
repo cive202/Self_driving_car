4956bbb4fc646b9bc54e45f522f179594943972d
cive202 <civecodes@gmail.com>
2025-10-13 15:20:14 +0545

    test 2

--- car.js
+++ car.js
@@ -1,93 +1,80 @@
 class Car{
     constructor(x,y,width,height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-       
-        this.speed = 0;
-        this.acclerataion = 0.2 
-        this.maxspeed = 3;
-        this.friction = 0.05;
-        this.angle = 0;
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed=3;
+        this.friction=0.05;
+        this.angle=0;
 
-        this.sensor = new Sensor(this)
-        this.control = new Controls();
-
+        this.sensor=new Sensor(this);
+        this.controls=new Controls();
     }
-    draw(ctx){
-        ctx.save()
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle)
-        ctx.beginPath();//Start a new drawing path — forget any previous shapes I was drawing
-        ctx.rect(
-            - this.width/4,
-            -this.height/4-22,
-            this.width/2,
-            this.height
-        );
-        ctx.fillStyle = "blue";     // Set fill color
-        ctx.fill();
 
+    update(roadBorders){
+        this.#move();
+        this.sensor.update(roadBorders);
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
+    }
+
+    draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
+        ctx.beginPath();
         ctx.rect(
-            - this.width/2,
+            -this.width/2,
             -this.height/2,
             this.width,
             this.height
         );
-        ctx.fillStyle = "black";     // Set fill color
-
         ctx.fill();
 
-
         ctx.restore();
-        
-        this.sensor.draw(ctx)
-    }
 
-    update(roadBoarders){
-        this.#move()
-        this.sensor.update(roadBoarders);
-    }
-
-    #move(){
-        if(this.control.forward){
-            this.speed +=this.acclerataion;
-        }
-        if(this.control.reverse){
-            this.speed -=this.acclerataion;
-        }
-        if(this.speed>this.maxspeed){
-            this.speed = this.maxspeed;
-        }
-        if(this.speed<-this.maxspeed/2){
-            this.speed =-this.maxspeed/2;
-        }
-       if(this.speed>0){
-           this.speed -= this.friction;
-       }
-       if(this.speed<0){
-           this.speed +=this.friction;
-       }
-       if(Math.abs(this.speed)<this.friction){
-        this.speed = 0;
-       }
-       if(this.speed!=0){
-        const flip = this.speed>0?1:-1;
-
-       
-
-            if(this.control.left){
-                this.angle += 0.03*flip;
-            }
-            if(this.control.right){
-                this.angle -= 0.03*flip;
-       }
-    }
-       this.x -=Math.sin(this.angle)*this.speed;
-       this.y -=Math.cos(this.angle)*this.speed;
+        this.sensor.draw(ctx);
     }
 }
-
-        
--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-const canvas = document.getElementById("myCanvas")
-canvas.height = window.innerHeight;
-canvas.width = 200;
-
+const canvas=document.getElementById("myCanvas");
+canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-// getContex("2d") returns a class which has all the 2d drawing tools
-const road = new Road (canvas.width/2,canvas.width*0.9)
-const car = new Car(road.getLaneCenter(1),100,30,50);
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50);
 
-animate();  
+animate();
 
 function animate(){
-    car.update();
-    canvas.height = window.innerHeight;
+    car.update(road.borders);
+
+    canvas.height=window.innerHeight;
 
     ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7)
+    ctx.translate(0,-car.y+canvas.height*0.7);
+
     road.draw(ctx);
     car.draw(ctx);
-    ctx.restore()
+
+    ctx.restore();
     requestAnimationFrame(animate);
-}
-
-
-
+}   
