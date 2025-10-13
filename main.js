cacb2fd3f43fa23ca3d7d39ba39be738899e5bdb
cive202 <civecodes@gmail.com>
2025-10-13 18:02:04 +0545

    defining-car-class

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-// let canvas = document.getElementById("myCanvas");
-// canvas.height = window.innerHeight; 
-// canvas.width = 200;
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight; 
+canvas.width = 200;
 
-// const ctx = 
+const ctx = canvas.getContext("2d")
+const car = new Car(100,100,30,50)
+car.draw(ctx);
