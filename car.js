4182ad99c5ac4c1df834049be8539876b8ae0854
cive202 <civecodes@gmail.com>
2025-10-15 03:14:44 +0545

    removing the plain rectangle and replacing by a defined point polygon aka a rectangle

--- car.js
+++ car.js
@@ -16,10 +16,43 @@ class Car{
     }
 
     update(roadBorders){
-        this.#move();
+        this.#move()
+        this.polygon = this.#createpolygon();
         this.sensor.update(roadBorders);
     }
 
+    //here we are defining the corners for the car, before we had only the center point. we want to show the collison so we want to define every corner of the car  
+#createpolygon(){
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+
+    // top-right
+    points.push({
+        x: this.x - Math.sin(this.angle - alpha) * rad,
+        y: this.y - Math.cos(this.angle - alpha) * rad
+    });
+
+    // top-left
+    points.push({
+        x: this.x - Math.sin(this.angle + alpha) * rad,
+        y: this.y - Math.cos(this.angle + alpha) * rad
+    });
+
+    // bottom-left
+    points.push({
+        x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+        y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+    });
+
+    // bottom-right
+    points.push({
+        x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+        y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+    });
+
+    return points;
+}
     #move(){
         if(this.controls.forward){
             this.speed+=this.acceleration;
@@ -60,21 +93,15 @@ class Car{
     }
 
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i=1; i<this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
 
-        ctx.restore();
-
+        ctx.closePath();  // connect last point to first
+        ctx.fillStyle = "black"; 
+        ctx.fill()
         this.sensor.draw(ctx);
     }
 }
