f44b80b2702dada73be57ba50af4fa4752c886b2
cive202 <civecodes@gmail.com>
2025-10-18 04:28:13 +0545

    final-touch-up DONE

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height,controlType,maxSpeed=5){
+    constructor(x,y,width,height,controlType,maxSpeed=5,color="blue"){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -19,7 +19,21 @@ class Car{
             )
         }
         this.controls=new Controls(controlType);
+        this.img = new Image()
+        this.img.src = "car.png"
+        this.mask = document.createElement("canvas")
+        this.mask.width = width
+        this.mask.height = height
         
+        const maskCtx=this.mask.getContext("2d");
+        this.img.onload=()=>{
+            maskCtx.fillStyle=color;
+            maskCtx.rect(0,0,this.width,this.height);
+            maskCtx.fill();
+
+            maskCtx.globalCompositeOperation="destination-atop";
+            maskCtx.drawImage(this.img,0,0,this.width,this.height);
+        }
     }
 
     update(roadBorders,traffic){
@@ -132,23 +146,28 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx,drawSensors=false){
-        if(this.damaged){
-            ctx.fillStyle="grey"
-        }
-        else{
-            ctx.fillStyle ="black"
-        }
-        ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
-        for(let i=1; i<this.polygon.length; i++){
-            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    draw(ctx,drawSensor=false){
+        if(this.sensor && drawSensor){
+            this.sensor.draw(ctx);
         }
 
-        ctx.closePath();  // connect last point to first
-        ctx.fill()
-        if(this.sensor && drawSensors){
-        this.sensor.draw(ctx);
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+        if(!this.damaged){
+            ctx.drawImage(this.mask,
+                -this.width/2,
+                -this.height/2,
+                this.width,
+                this.height);
+            ctx.globalCompositeOperation="multiply";
         }
+        ctx.drawImage(this.img,
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height);
+        ctx.restore();
+        
     }
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N = 100
+const N = 1
 const cars  =generateCars(N);
 let bestCar = cars[0]
 
@@ -60,7 +60,7 @@ const traffic = [
     // 🔹 Deep tunnel challenge (tight cluster)
     new Car(road.getLaneCenter(0), -3500, 30, 50, "DUMMY", 2),
     new Car(road.getLaneCenter(1), -3510, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(1), -3530, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -3600, 30, 50, "DUMMY", 2),
 ];
 
 
